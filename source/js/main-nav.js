a2a9419c2cea5c4e38fdd0681b74c3528b422782
Olga Kravchenko <olga19972010@yandex.by>
2021-01-22 10:39:29 +0300

    5.1 Pixel Perfect in main-nav

---
+++ source/js/main-nav.js
@@ -0,0 +1,14 @@
+let nav = document.querySelector('.nav');
+let navToggle = document.querySelector('.header-nav__toggle');
+
+nav.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (nav.classList.contains('nav--closed')) {
+    nav.classList.remove('nav--closed');
+    nav.classList.add('nav--opened');
+  } else {
+    nav.classList.add('nav--closed');
+    nav.classList.remove('nav--opened');
+  }
+});
